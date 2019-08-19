595ad5cdbdb8665ff2d1d2957c52c15bb095e6ac
mcarneal <mcarneal09@yahoo.com>
2019-08-19 17:29:20 +0000

    created const for cart

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ const scrollTo = () => {
 
 
 
+
 $(window).scroll(()=> {
+    const miniCart = document.getElementsByClassName('mini-cart-product')
+
    if($(window).scrollTop() + $(window).height() > $(document).height() *.90  ) {
-       $(window).unbind('scroll');
-       alert("near bottom!");
+      $(window).unbind('scroll');
+      //alert("near bottom!");
+      for (let i = 0 ; miniCart.length > i ; i++){
+		console.log(miniCart[i])
+	}
    }
 });
+
+
+
