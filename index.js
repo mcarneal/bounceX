a9a6413520bc1b584529d1a9f9adafa15a023cfe
mcarneal <mcarneal09@yahoo.com>
2019-08-19 18:29:48 +0000

    found item name

--- index.js
+++ index.js
@@ -7,14 +7,16 @@ const scrollTo = () => {
 
 
 $(window).scroll(()=> {
-    const miniCart = document.getElementsByClassName('mini-cart-product')
-
+   const miniCart = document.getElementsByClassName('mini-cart-product')
+   let list = [] 
    if($(window).scrollTop() + $(window).height() > $(document).height() *.90  ) {
       $(window).unbind('scroll');
       //alert("near bottom!");
       for (let i = 0 ; miniCart.length > i ; i++){
-		console.log(miniCart[i])
-	}
+          let name = miniCart[i].getElementsByClassName('mini-cart-name')
+          list.push(name[0].innerText)
+      }
+       alert(list)
    }
 });
 
